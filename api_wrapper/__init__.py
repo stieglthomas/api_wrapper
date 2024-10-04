f90269663b943437a1095b6bb0ef7fc2bfaeb058
thxmxss <thxmxss@gmail.com>
2024-10-04 16:04:49 +0200

    added FreshRssAPI

--- api_wrapper/__init__.py
+++ api_wrapper/__init__.py
@@ -2,4 +2,4 @@ from .HassAPI import HassAPI
 from .GroqAPI import GroqAPI
 from .FreshRssAPI import FreshRssAPI
 
-__all__ = ["HassAPI", "GroqAPI", "FreshRRssAPI"]
+__all__ = ["HassAPI", "GroqAPI", "FreshRssAPI"]
